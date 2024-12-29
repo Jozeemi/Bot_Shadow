ec1f455e5ee539a6af66dec1d2c75f2d972456cc
Jozeemi <josemiguelrodriguezalmeida2008@gmail.com>
2024-12-29 22:48:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Edit the status", "**Jugando🎮 Shadow**"];
+const statusMessages = ["Jugando🎮 Shadow"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
